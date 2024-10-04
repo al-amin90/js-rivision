da37636b6fbf4d1c0dc4ed08e8a90bb88fe76a66
al-amin90 <ijesun30@gmail.com>
2024-10-04 10:31:39 +0600

    add string fun

--- storage.js
+++ storage.js
@@ -11,10 +11,27 @@ addToLocalStorage = () => {
 
 function generateHashtag (str) {
     if(!str) return false;
+    console.log(!str.trim());
 
-      const withHash = str.split(" ").map(s => s.charAt(0).toUpperCase() + s.slice(1)).join("")
+      const withHash = "#" + str.split(" ").map(s => s.charAt(0).toUpperCase() + s.slice(1)).join("")
       return withHash.length > 140 ? false : withHash
 
   }
-  const output = generateHashtag("thsnk for thi s    s fs");
-  console.log(output);
+  const output = generateHashtag("");
+
+  const greet = "hello world";
+  const phonecall = greet.substring(5, 2);
+  // console.log(phonecall);
+  const phonecall2 = greet.slice( 5, 2);
+  // console.log(phonecall2);
+
+  const id = 3;
+  const fillId = id.toString().padStart(6, "0");
+  // console.log(fillId);
+
+  const orderNo = 1;
+  const firstOrder = orderNo.toString().padStart(4, "0")
+
+  const secondOrder = Number(firstOrder.substring(3)) +1;
+  const secondOrderId = secondOrder.toString().padStart(4, "0")
+  console.log(secondOrderId);
