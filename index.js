1d8500b8a250698625d1f2bace034ac1d3f8e8b4
al-amin90 <ijesun30@gmail.com>
2024-11-22 17:58:23 +0600

    mongooess operator

---
+++ index.js
@@ -0,0 +1,40 @@
+const orderNo = 1;
+const firstOrder = orderNo.toString().padStart(4, "0");
+const secondOrder = (parseInt(firstOrder) + 1).toString().padStart(4, "0")
+// console.log(secondOrder);
+
+let mangoPot = 0;
+
+for(let i = 1; i <= 3; i++){
+    mangoPot = mangoPot + 1;
+}
+// console.log(mangoPot);
+
+const names = ['mir', 'mezba', 'jisan', 'kuddus'];
+
+for(let i=0 ; i < names.length; i++){
+    // console.log(`Welcome to level2 ${names[i]}`);
+}
+
+for(let mentor of names){
+    // console.log(`Welcome to level2 ${mentor}`);
+}
+
+const mentor = {
+    name: "mir",
+    age: 23,
+    isMentor: true
+}
+for(let key in mentor){
+    // console.log(mentor[key]);
+}
+// console.log(mentor["age"]);
+
+
+function randomFunction(fn){
+    return fn(2)
+}
+
+randomFunction((number) => {
+    console.log(3 + number);
+})
---
+++ mongo.js
@@ -0,0 +1,64 @@
+—-------------------------------- Fill filtering 2 vabe kora jai —--------------------------------
+db.test.insert( {name: "Something”} )
+db.test.insertMany([
+    {name: "Complete Web Development"},
+    {name: "Level 2 web development"}
+    ])
+db.test.find({gender: "Male"}, {name: 1, email: 1, phone: 1,  gender: 1})
+db.test.find({gender: "Male"}).project({name:1, gender: 1}) // → a method findOne kaj kore na
+
+—-------------------------------- Mongoose operator —--------------------------------
+db.test.find({age: {$eq: 12}}) → operator use ar tum rule je {} use kora
+db.test.find({age: {$ne: 12}})  → not equal
+db.test.find({age: {$gt: 30 }}) → gether tha 
+db.test.find({age: {$lte: 30 }}).sort({age: -1}) → sort decending
+db.test.find({age: {$lt: 30 }}) → less than
+db.test.find({age: {$lte: 30 }})  → less than equal
+
+db.test.find(
+    { 
+        gender: { $eq: "Female" }, 
+        age: {$gte: 18 , $nin : [18, 20, 22, 26, 30] } ,
+        interests: {$in: ["Cooking", "Gaming"] }
+    },
+    { age: 1, gender: 1 , interests: 1}  → implicit and , coma diye and korci
+).sort({ age: 1 })
+
+—------------------------------------------ Explicit and —-------------------------------------------
+db.test.find({ $and: [
+      {gender: "Female"},
+      {age: {$ne: 15}}, 
+      {age: {$lte: 30}}
+    ] 
+    
+}).project({
+    age: 1, 
+    gender: 1
+}).sort({age: 1})
+
+—------------------------------------------ Explicit $or—-------------------------------------------
+db.test.find({ 
+    $or: [ → chaile implicit $or ar jonno $in use korte pari
+      {"skills.name": "JAVASCRIPT"},
+      {"skills.name": "PYTHON"},
+    ] 
+    
+}).project({
+    skills: 1
+})
+
+—------------------------------------------ not operator—-------------------------------------------
+db.test.find({
+    age:
+        { $not: { $ne: 15 } } —-------→ ja bola hobe tar ulta ta korbe
+}).project({ age: 1 })
+
+—------------------------------------------ nor operator—-------------------------------------------
+db.test.find({
+    $nor: [      —---------→ agula bad a sob gula dibe
+        {"skills.name": "JAVASCRIPT"},
+        {"skills.name": "RUBY"}
+    ]
+}).project({
+    skills: 1
+})
