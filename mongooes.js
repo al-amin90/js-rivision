867551f646c4d991722e5d95a4a31cee0b1962cb
al-amin90 <ijesun30@gmail.com>
2024-11-29 22:58:00 +0600

    add: mongoess push/pop/pull/inc/set advance

--- mongooes.js
+++ mongooes.js
@@ -72,9 +72,64 @@ db.test.updateOne(
         $addToSet: {
             interests: "Sleeping"}
             interests: {$each: ["Praying", "Power Nap"]} // aker odik add korte chaile
-        }
+        },
         $push: {
             interests: {$each: ["Praying", "Power Nap"]} // aker odik push korte chaile
         }
     }
 )
+
+//kono property ke unset ba delete kora $unset use
+db.test.updateOne(
+    {_id: ObjectId("6406ad63fc13ae5a40000065") }, 
+    {$unset: {age: 1 }}
+    )
+
+//arr pop -1 korle first theke and 1 last
+db.test.updateOne(
+    {_id: ObjectId("6406ad63fc13ae5a40000065") }, 
+    {$pop: {friends: 1 }}
+    )
+
+//pull kore delete kora
+db.test.updateOne(
+    {_id: ObjectId("6406ad63fc13ae5a40000065") }, 
+    {$pull: {friends: "Fahim Ahammed Firoz" }}
+    )
+
+    //object ar ketre set advance used
+db.test.updateOne(
+    {_id: ObjectId("6406ad63fc13ae5a40000065") }, 
+    {$set: {
+        "address.city": "dhaka",
+        "address.country": "bangladesh",
+        "address.postalCode": "000"
+    } }
+    )
+
+//array of object update korar jonno use korar jonno
+db.test.updateOne(
+    { _id: ObjectId("6406ad63fc13ae5a40000065"), "education.major": "Communications" },
+    {
+        $set: {
+            "education.$.year": 2000
+        }
+    }
+)
+
+//increment property
+db.test.updateOne(
+    { _id: ObjectId("6406ad63fc13ae5a40000065") },
+    {
+        $inc: {
+            age: 1
+        }
+    }
+)
+
+// create collection
+db.createCollection("posts")
+
+// delete collection
+db.posts.drop( { writeConcern: { w: 1 } } )
+
